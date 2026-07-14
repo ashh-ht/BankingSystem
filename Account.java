daeb0b563aafd78b188884e7d4ce80d8d34fee51
FRANCIS FERNANDEZ <ffernandez.itso@tip.edu.ph>
2026-07-14 13:27:56 +0800

    GAWIN NYO TOH

---
+++ Account.java
@@ -0,0 +1,39 @@
+public class Account {
+    private String firstName;
+    private String lastName;
+    private int cardNum;
+    private int cardPin;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getCardNum() {
+        return cardNum;
+    }
+
+    public void setCardNum(int cardNum) {
+        this.cardNum = cardNum;
+    }
+
+    public int getCardPin() {
+        return cardPin;
+    }
+
+    public void setCardPin(int cardPin) {
+        this.cardPin = cardPin;
+    }
+    
+}
--- Methods.java
+++ Methods.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class Methods {
     Scanner sc = new Scanner(System.in);
-    //withdraw
-    //deposit
-    //transfer
-    //view balance and acc details
+    //FEATURES
+    //1. withdraw
+        //-minimum of 100, maximum of 20,000 pesos
+    //2. deposit
+        //-minimum of 100, maximum of 20,000 pesos
+    //3. transfer
+        //-minimum of 50, maximum of 10,000 pesos
+    //4. view balance and account details
+        //-acc name
+        //-card number
+        //-balance
+    //5. edit card details
+    //6. history
+
 
 
     //login
