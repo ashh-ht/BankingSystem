46cc7900cb8e5dd5d1b75696101e552a6f18c570
ashh-ht <qshsulla@tip.edu.ph>
2026-07-18 21:32:53 +0800

    hello

--- Methods.java
+++ Methods.java
@@ -4,83 +4,6 @@ public class Methods {
     Scanner sc = new Scanner(System.in);
     private Account account;
 
-    // WITHDRAWAL FUNCTION - Hendrick
-        public void Withdraw(double Amount ){
-        // i created this to create a loop to avoid error on withdrawal and Deposit
-        boolean Validity = false;
-        System.out.println("~~~~~~~~~~Withdrawal~~~~~~~~~~");
-        
-        
-        //Error handling  this loop makes that whenever a user tries to use anything but numbers will be put in a loop and catch error to prevent crash
-        while(!Validity){
-            System.out.println("Please Enter the amount: ");
-            try{
-            Amount = sc.nextDouble();
-            Validity = true;
-        }catch(InputMismatchException ex){
-            System.out.println("Invalid number input, try again.");
-            sc.nextLine();
-        }
-    }
-        
-        // If else Deposit thingy basically I just change the operation on the setbalance
-        if( Amount < 100){
-            System.out.println("The minimum required to withdraw is Php 100:  " + account.getBalance());
-        } 
-        else if( Amount > 20000){
-            System.out.println("The maximum required to withdraw is Php 20,000:  " + account.getBalance());
-        }
-        else if ( Amount > account.getBalance()){
-            System.out.println("Insufficient credits Your current Balance:  " + account.getBalance());
-        }
-        else{
-            // Subtract the balance of the user to the amount that was inputted then updates it using the setBalance
-            account.setBalance(account.getBalance() - (double)Amount);
-            System.out.printf("~~~ Withdraw Successful ~~~%nAmount Deposited: %.2f%nNew Balance: %.2f%n", Amount, account.getBalance());
-            }
-        
-            }
-            
-        
-        
-    // DEPOSIT FUNCTION - Hendrick
-            public void Deposit(double Amount ){
-        // i created this to create a loop to avoid error on withdrawal and Deposit
-        boolean Validity = false;
-        System.out.println("~~~Depositing~~~");
-        
-        
-        //Error handling  this loop makes that whenever a user tries to use anything but numbers will be put in a loop and catch error to prevent crash
-        while(!Validity){
-            System.out.println("Please Enter the amount: ");
-            try{
-            Amount = sc.nextDouble();
-            Validity = true;
-        }catch(InputMismatchException ex){
-            System.out.println("Invalid number input, try again.");
-            sc.nextLine();
-        }
-    }
-        
-        // If else Deposit thingy
-        if( Amount < 100){
-            System.out.println("The minimum required to deposit is Php 100:  " + account.getBalance());
-        } 
-        else if( Amount > 20000){
-            System.out.println("The maximum required deposit is Php 20,000:  " + account.getBalance());
-        }
-        else if ( Amount > account.getBalance()){
-            System.out.println("Insufficient credits Your current Balance:  " + account.getBalance());
-        }
-        else{
-            // add the balance of the user to the amount that was inputted then updates it using the setBalance
-            account.setBalance(account.getBalance() + (float)Amount);
-            //formatted into two decimal place
-            System.out.printf("~~~ Deposit Successful ~~~%nAmount Deposited: %.2f%nNew Balance: %.2f%n", Amount, account.getBalance());
-            }
-        
-            }
-        
     // 3. transfer
     // -minimum of 50, maximum of 10,000 pesos
     // 4. view balance and account details
@@ -89,9 +12,6 @@ public class Methods {
     // -balance
     // 5. edit card details
     // 6. history
-
-
-
 
     private final Map<String, Account> dummyData = new HashMap<>(Map.of(
             "1234567899876543", new Account("1234567899876543", "Sylvia Heart", "Sulla", "1234", 100000),
@@ -235,10 +155,10 @@ public class Methods {
 
             switch (choice) {
                 case 1:
-                    withdraw();
+                    this.Withdraw(0);
                     break;
                 case 2:
-                    deposit();
+                    this.Deposit(0);
                     break;
                 case 3:
                     transfer();
